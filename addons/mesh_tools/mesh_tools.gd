5f87ecfebbd2c785f699d6b1df066ee34a47f3b2
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2026-05-14 20:28:56 -0500

    Added documentation

--- addons/mesh_tools/mesh_tools.gd
+++ addons/mesh_tools/mesh_tools.gd
@@ -2,14 +2,17 @@ class_name MeshTools
 
 
 ## Islands has the methods:
-## separate_mesh_islands
+## [br]separate_mesh_islands
+## [br]    Takes a [param mesh], and returns an array of all the floating mesh islands separated out of it.
+## [br]calculate_mesh_volume
+## [br]    Returns the given [param mesh]'s volume, in the cubed unit of said mesh. (typically cubic meters) It is assumed the mesh is watertight.
 const Islands = preload("res://addons/mesh_tools/scripts/island_separate.gd")
 
 ## CleanUp has the methods:
 ## [br]rebuild_csg_node
 ## [br]    Bakes any CSG node and all it's children into one new [CSGMesh3D].  The children are then removed after.
 ## [br]merge_by_distance
-## [br]    Takes a mesh and performs a merge by distance operation on it, useful after a CSG operation.
+## [br]    Takes a [param mesh] and performs a merge by distance operation on it, useful after a CSG operation.
 const CleanUp = preload("res://addons/mesh_tools/scripts/mesh_cleanup.gd")
 
 ## BodyProblems has the methods:
