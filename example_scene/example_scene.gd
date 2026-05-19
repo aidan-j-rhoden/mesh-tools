85a65ad50f42cfb8a59d11ae87276b9f37eb6f2b
Aidan Rhoden <aidan.j.rhoden@gmail.com>
2026-05-19 16:41:52 -0500

    Updated documentation, style guide updates

--- example_scene/example_scene.gd
+++ example_scene/example_scene.gd
@@ -1,26 +1,26 @@
 extends Node3D
 
 func _ready() -> void:
-	await get_tree().create_timer(5).timeout
 	the_test()
 
 
 # Called when the node enters the scene tree for the first time.
 func the_test() -> void:
+	print("Started tests")
 	await MeshTools.CleanUp.rebuild_csg_node($CSGBox3D/CSGSphere3D5, true) # Compile the CSG node tree, and ALL it's children into one CSGMesh node
-	print("first one finished")
+	print("Rebuilt CSG tree")
 
 	# Merge the newly compiled mesh by distance, cleaning up geometry by removing doubled vertices.
 	$CSGBox3D.mesh = await ThreadRunner.run_async(MeshTools.CleanUp.merge_by_distance, [$CSGBox3D.mesh, 0.02])
-	print("second one finished")
+	print("Merged the mesh vertices by distance")
 
 	# Separate the big mesh by floating islands
-	var separated_meshes = await ThreadRunner.run_async(MeshTools.Islands.separate_mesh_islands, [$CSGBox3D.mesh, true])
-	print("third one finished")
+	var separated_meshes: Array[Mesh] = await ThreadRunner.run_async(MeshTools.Islands.separate_mesh_islands, [$CSGBox3D.mesh, true])
+	print("Separated the mesh by islands")
 
 	# For each separated mesh, create a new rigid body and add it to the scene
 	for m in separated_meshes:
-		var mi = MeshTools.BodyProblems.create_rigid_body_from_mesh(
+		var mi: RigidBody3D = MeshTools.BodyProblems.create_rigid_body_from_mesh(
 			m, # The mesh
 			1.0, # Friction
 			0.3, # Bounciness
@@ -31,8 +31,9 @@ func the_test() -> void:
 		# Make sure to set the center of mass of any new mass, as it defaults to the mesh origin, and these all have the same origin.  If left unalterd, physics will be comedically wrong.
 		MeshTools.BodyProblems.set_center_of_mass(mi, true)
 		# Perhaps you want to make sure we're not preserving any islands that are too small?  This checks the volume.
-		print(MeshTools.Islands.calculate_mesh_volume(m))
-	print("forth one finished")
+		if MeshTools.Islands.calculate_mesh_volume(m) <= 0.1:
+			mi.queue_free()
+	print("Created rigid bodies from mesh islands")
 	$CSGBox3D.queue_free() # We don't need this anymore, but only delete it after everything else is done for visuals.  This runs in one go, and therefore doesn't matter, but it's good practice.
 
 	# Random tests on the random sphere
